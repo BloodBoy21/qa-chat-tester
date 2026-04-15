484e9b421c9b32b6f3f6c784b732d407fc9b5db6
Alan B. <alan@nerds.ai>
2026-04-15 12:42:58 -0600

    feat: update MongoDB client initialization to conditionally use TLS CA file

--- lib/mongo.py
+++ lib/mongo.py
@@ -7,6 +7,8 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DB: str = os.getenv("MONGO_DB", "qa_chat_tester")
 
-
-client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+TSL_CA_FILE = (
+    certifi.where() if os.getenv("MONGO_TLS", "false").lower() == "true" else None
+)
+client = MongoClient(MONGO_URI, tlsCAFile=TSL_CA_FILE)
 db = client[MONGO_DB]
